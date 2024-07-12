60a46eff111317f0e1e5badfa3b29a9d672d3b66
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-07-12 16:47:31 +0800

    make calculator unable to pass the test and edit pipeline 26

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const DB_PWD = process.env.DB_PWD;
 
 app.get('/', (req, res) => {
-  res.send('Hello Pipeline 25');
+  res.send('Hello Pipeline 26');
 });
 
 app.get('/db', (req, res) => {
